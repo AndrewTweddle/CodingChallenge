4924682387d61f7021c2f3c0ad1c96f192773d3f
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2014-01-06 21:34:33 +0200

    Unit tests to match family and model separately

--- python/recordlinker/test/builder_tests.py
+++ python/recordlinker/test/builder_tests.py
@@ -1,6 +1,12 @@
 import unittest
 from recordlinker.builder import *
 
+class FamilyAndModelSeparatelyMasterTemplateBuilder(BaseMasterTemplateBuilder):
+    def get_listing_templates(self):
+        return self.generate_listing_templates_from_methods(
+            [BaseMasterTemplateBuilder.match_family_and_model_separately_with_regex]
+        )
+
 class MasterTemplateBuilderTestCase(unittest.TestCase):
     def setUp(self):
         pass
@@ -33,7 +39,78 @@ class MasterTemplateBuilderTestCase(unittest.TestCase):
         self.assert_(match_result.is_match, 'A match should be found')
         self.assertEqual(match_result.match_value, expected_match_value)
         self.assertEqual(match_result.description, expected_description)
+    
+    def testFamilyAndModelSeparately(self):
+        classification = 'a+an'
+        blocks = ['Coolpix', '+','S','6100']
+        product_desc = 'Coolpix with code S6100'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(match_result.is_match, 'Check match of family and model separately')
+    
+    def testFamilyAndModelSeparatelyWithOnlyFamilyMatched(self):
+        classification = 'a+an'
+        blocks = ['Coolpix', '+','S','6100']
+        product_desc = 'Coolpix 900S'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'There should be no match if only family is found')
+    
+    def testFamilyAndModelSeparatelyWithOnlyFamilyMatched(self):
+        classification = 'a+an'
+        blocks = ['Coolpix', '+','S','6100']
+        product_desc = 'Hotpix S6100'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'There should be no match if only model is found')
+    
+    def testFamilyAndModelSeparatelyWithFamilyClassificationEmpty(self):
+        classification = '+a-a_n'
+        blocks = ['+','V','-','LUX',' ','20']
+        product_desc = 'V-LUX 20'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the family is empty')
+    
+    def testFamilyAndModelSeparatelyWithNoAlphaInModel(self):
+        classification = 'a-a+n'
+        blocks = ['D-Lux','+','5']
+        product_desc = 'D-Lux 5'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the model contains no alphabetic characters')
 
+    def testFamilyAndModelSeparatelyWithNoNumericsInModel(self):
+        classification = 'a+a'
+        blocks = ['DigiLux','+','Zoom']
+        product_desc = 'DigiLux Zoom'
+        extra_prod_details = ''
+        
+        builder = FamilyAndModelSeparatelyMasterTemplateBuilder(classification)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the model contains no digits')
 
 # Run unit tests from the command line:        
 if __name__ == '__main__':
