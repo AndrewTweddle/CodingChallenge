a999c030e74f448284f04cf9dcd0854452f19ccc
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2014-01-07 21:39:28 +0200

    New listing template: model + words in family
    1. Add
    BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex()
    to build ListingMatcherTemplate to match on model and optionally on
    individual words in family.
    Constraints: family must have at least 2 words, model must contain both
    alphabetic and numeric characters.
    2. Add unit tests.
    3. Added SingleMethodMasterTemplateBuilder to simplify setting up a
    master template builder that runs a single method only.

--- python/recordlinker/builder.py
+++ python/recordlinker/builder.py
@@ -1,4 +1,5 @@
 from recordlinker.classification import *
+import re
 # from pdb import set_trace
 
 # --------------------------------------------------------------------------------------------------
@@ -7,12 +8,18 @@ from recordlinker.classification import *
 class BaseMasterTemplateBuilder(object):
     all_of_family_and_model_with_regex_desc = 'Family and model approximately'
     family_and_model_separately_with_regex_desc = 'Family and model separately and approximately'
+    model_and_words_in_family_with_regex_desc = 'Model and words in family approximately'
     
     all_of_family_and_model_with_regex_value_func_on_prod_desc = MatchValueFunction( 1000000, 10000)
     all_of_family_and_model_with_regex_value_func_on_prod_details = MatchValueFunction( 10000, 100)
     family_and_model_separately_with_regex_value_func_on_prod_desc = MatchValueFunction( 250000, 2500)  # NB: These will be added twice - once for family and once for model
     family_and_model_separately_with_regex_value_func_on_prod_details = MatchValueFunction( 2500, 25)  # NB: These will be added twice - once for family and once for model
+    # model_and_words_in_family uses the value functions above for the model match, and the value functions below for each word match:
+    family_word_with_regex_value_func_on_prod_desc = MatchValueFunction( 10000, 100)  # NB: These will be added twice - once for family and once for model
+    family_word_with_regex_value_func_on_prod_details = MatchValueFunction( 100, 1)  # NB: These will be added twice - once for family and once for model
     
+    word_regex_pattern = '(?:[can]|\-)+'
+        
     def __init__(self, classific):
         self.classification = classific
         sep_index = classific.index('+')
@@ -21,6 +28,7 @@ class BaseMasterTemplateBuilder(object):
         self.family_classification = classific[self.family_slice]
         self.model_slice = slice( sep_index + 1, len(classific))
         self.model_classification = classific[self.model_slice]
+        self.word_regex = re.compile( BaseMasterTemplateBuilder.word_regex_pattern, re.IGNORECASE | re.UNICODE | re.VERBOSE )
         # TODO: Calculate long_prod_code_slices, short_prod_code_slices, alternate_prod_code_slices, secondary_prod_code_slices
     
     @abstractmethod
@@ -79,15 +87,74 @@ class BaseMasterTemplateBuilder(object):
             [family_rule_tpl, model_rule_tpl], [])
         return [listing_tpl]
 
+    def match_model_and_words_in_family_with_regex(self):
+        # Check that family is not empty:
+        if len(self.family_classification) == 0:
+            return []
+        
+        # Check that model has both alphas and numerics.
+        # This is to avoid matching "Digilux" "4.3", "Digilux" "Zoom", "D-Lux" "5":
+        modelc = self.model_classification
+        has_no_alpha = modelc.find('a') == -1 and modelc.find('c') == -1
+        has_no_numeric = modelc.find('n') == -1
+        if has_no_alpha or has_no_numeric:
+            return []
+        
+        # Check that family has multiple words i.e. it contains whitespace:
+        index_of_whitespace = self.family_classification.find('_', 1)  # Start at index 1 to ignore leading whitespace
+        famc_len = len(self.family_classification)
+        if index_of_whitespace == -1 or index_of_whitespace == famc_len - 1:
+            return []
+        
+        # Find words in family:
+        start_index = 0
+        word_slices = []
+        while True:
+            word = self.word_regex.search(self.family_classification, start_index)
+            if word == None:
+                break
+            word_slice = slice(word.start(), word.end())
+            word_slices.append(word_slice)
+            start_index = word.end()
+        
+        # Generate the listing template:
+        model_rule_tpl = RegexRuleTemplate( [self.model_slice],
+            MasterTemplateBuilder.family_and_model_separately_with_regex_value_func_on_prod_desc,
+            MasterTemplateBuilder.family_and_model_separately_with_regex_value_func_on_prod_details,
+            must_match_on_desc = True)
+        family_word_rule_tpls = [
+            RegexRuleTemplate( [word_slice], 
+                MasterTemplateBuilder.family_word_with_regex_value_func_on_prod_desc,
+                MasterTemplateBuilder.family_word_with_regex_value_func_on_prod_details,
+                must_match_on_desc = False)
+            for word_slice in word_slices
+        ]
+        listing_tpl = ListingMatcherTemplate(
+            MasterTemplateBuilder.model_and_words_in_family_with_regex_desc,
+            [model_rule_tpl], family_word_rule_tpls)
+        return [listing_tpl]
+
 # --------------------------------------------------------------------------------------------------
 # A derived class which is the standard way to build a MasterTemplate from a classification string:
 # 
 class MasterTemplateBuilder(BaseMasterTemplateBuilder):
     default_listing_template_methods = [
         BaseMasterTemplateBuilder.match_all_of_family_and_model_with_regex,
-        BaseMasterTemplateBuilder.match_family_and_model_separately_with_regex
+        BaseMasterTemplateBuilder.match_family_and_model_separately_with_regex,
+        BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex
         # TODO: Extend with more methods
     ]
     
     def get_listing_templates(self):
         return self.generate_listing_templates_from_methods(MasterTemplateBuilder.default_listing_template_methods)
+
+# --------------------------------------------------------------------------------------------------
+# A derived class using a single method to build a MasterTemplate from a classification string:
+# 
+class SingleMethodMasterTemplateBuilder(BaseMasterTemplateBuilder):
+    def __init__(self, classific, single_method):
+        BaseMasterTemplateBuilder.__init__(self, classific)
+        self.single_list_template_method = single_method
+    
+    def get_listing_templates(self):
+        return self.generate_listing_templates_from_methods([self.single_list_template_method])
--- python/recordlinker/test/builder_tests.py
+++ python/recordlinker/test/builder_tests.py
@@ -1,16 +1,12 @@
 import unittest
 from recordlinker.builder import *
 
-class FamilyAndModelSeparatelyMasterTemplateBuilder(BaseMasterTemplateBuilder):
-    def get_listing_templates(self):
-        return self.generate_listing_templates_from_methods(
-            [BaseMasterTemplateBuilder.match_family_and_model_separately_with_regex]
-        )
+class FamilyAndModelSeparatelyMasterTemplateBuilder(SingleMethodMasterTemplateBuilder):
+    def __init__(self, classific):
+        SingleMethodMasterTemplateBuilder.__init__(self, classific, 
+            BaseMasterTemplateBuilder.match_family_and_model_separately_with_regex)
 
 class MasterTemplateBuilderTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
-    
     def testBuilderInit(self):
         classification = 'a-a+a-an'
         builder = MasterTemplateBuilder(classification)
@@ -23,6 +19,7 @@ class MasterTemplateBuilderTestCase(unittest.TestCase):
         self.assertEqual(builder.model_slice.start, 4)
         self.assertEqual(builder.model_slice.stop, 8)
     
+class AllOfFamilyAndModel_MasterTemplateBuilderTestCase(unittest.TestCase):
     def testAllOfFamilyAndModelApproximately(self):
         classification = 'a-a+a-an'
         blocks = ['Cyber', '-', 'shot', ' ', 'DSC', '-', 'W', '310']
@@ -40,6 +37,7 @@ class MasterTemplateBuilderTestCase(unittest.TestCase):
         self.assertEqual(match_result.match_value, expected_match_value)
         self.assertEqual(match_result.description, expected_description)
     
+class FamilyAndModelSeparately_MasterTemplateBuilderTestCase(unittest.TestCase):
     def testFamilyAndModelSeparately(self):
         classification = 'a+an'
         blocks = ['Coolpix', '+','S','6100']
@@ -111,6 +109,109 @@ class MasterTemplateBuilderTestCase(unittest.TestCase):
         engine = master_tpl.generate(blocks)
         match_result = engine.try_match_listing(product_desc, extra_prod_details)
         self.assert_(not match_result.is_match, 'No match should be found since the model contains no digits')
+
+class ModelAndWordsInFamily_MasterTemplateBuilderTestCase(unittest.TestCase):
+    def testModelAndWordsInFamily(self):
+        classification = 'a_a+n_c'
+        blocks = ['Digital', ' ', 'IXUS', '+', '1000', ' ', 'HS']
+        product_desc = 'IXUS 1000 HS from Digital'
+        extra_prod_details = 'Digital camera'
+        
+        expected_match_value \
+            = BaseMasterTemplateBuilder.family_and_model_separately_with_regex_value_func_on_prod_desc.evaluate(len('1000 HS')) \
+            + BaseMasterTemplateBuilder.family_word_with_regex_value_func_on_prod_desc.evaluate(len('Digital')) \
+            + BaseMasterTemplateBuilder.family_word_with_regex_value_func_on_prod_desc.evaluate(len('IXUS')) \
+            + BaseMasterTemplateBuilder.family_word_with_regex_value_func_on_prod_details.evaluate(len('Digital'))
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(match_result.is_match, 'Check match of model and words in family')
+        self.assertEqual(match_result.match_value, expected_match_value)
+    
+    def testModelAndWordsInFamilyWithOnlyModelMatched(self):
+        classification = 'a_a+n_c'
+        blocks = ['Digital', ' ', 'IXUS', '+', '1000', ' ', 'HS']
+        product_desc = '1000 HS'
+        extra_prod_details = ''
+        
+        expected_match_value \
+            = BaseMasterTemplateBuilder.family_and_model_separately_with_regex_value_func_on_prod_desc.evaluate(len('1000 HS'))
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(match_result.is_match, 'There should still be a match if only model is found')
+        self.assertEqual(match_result.match_value, expected_match_value)
+    
+    def testModelAndWordsInFamilyWithOnlyFamilyWordsMatched(self):
+        classification = 'a_a+n_c'
+        blocks = ['Digital', ' ', 'IXUS', '+', '1000', ' ', 'HS']
+        product_desc = 'Digital IXUS'
+        extra_prod_details = '1000 HS'
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'There should be no match if only family words are found')
+    
+    def testModelAndWordsInFamilyWithFamilyHavingOnlyOneWord(self):
+        classification = '+a-a_n'
+        blocks = ['+','V','-','LUX',' ','20']
+        product_desc = 'V-LUX 20'
+        extra_prod_details = ''
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the family is empty')
+    
+    def testModelAndWordsInFamilyWithOnlyOneWordInFamily(self):
+        classification = 'a_+an'
+        blocks = ['Cybershot',' ','+','W','580']
+        product_desc = 'Cybershot W580'
+        extra_prod_details = ''
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the family field has only one word')
+
+    def testModelAndWordsInFamilyWithNoAlphaInModel(self):
+        classification = 'a_a+n'
+        blocks = ['D',' ','Lux','+','5']
+        product_desc = 'D Lux 5'
+        extra_prod_details = ''
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the model contains no alphabetic characters')
+
+    def testModelAndWordsInFamilyWithNoNumericsInModel(self):
+        classification = 'a_a+a'
+        blocks = ['Digi', ' ','Lux','+','Zoom']
+        product_desc = 'Digi Lux Zoom'
+        extra_prod_details = ''
+        
+        builder = SingleMethodMasterTemplateBuilder(classification, 
+            BaseMasterTemplateBuilder.match_model_and_words_in_family_with_regex)
+        master_tpl = builder.build()
+        engine = master_tpl.generate(blocks)
+        match_result = engine.try_match_listing(product_desc, extra_prod_details)
+        self.assert_(not match_result.is_match, 'No match should be found since the model contains no digits')
+
 
 # Run unit tests from the command line:        
 if __name__ == '__main__':
