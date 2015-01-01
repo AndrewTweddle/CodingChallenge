7828ff8b467b461455cdbf69d3987809bd7925ee
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-01-01 21:45:44 +0200

    Modified comparisons to ignore duplicates
    Modified the comparison.py script to:
    a. Optionally ignore the last character of each line (a hack because of
    how Python 2 treats line endings)
    b. Ignore listings with duplicate titles (even if other fields like
    currency and price were different). This made comparisons more
    meaningful as the comparison does an outer join on title, which was
    producing too many duplicate matches.

--- python/comparison.py
+++ python/comparison.py
@@ -3,8 +3,11 @@ import json
 from pandas import DataFrame, Series
 import pandas as pd
 
-def load_results(json_file_path):
-  lines = [json.loads(line) for line in open(json_file_path)]
+def load_results(json_file_path, new_line_offset):
+  lines = [ json.loads(line[0:len(line) - new_line_offset]) # Filter out newline character at the end of each line
+            for line in open(json_file_path, 'r') 
+            if len(line) > 0  # Filter out possible final blank line
+          ]
   joins = [ { 'product_name': line['product_name'],
               'listing_manufacturer': listing['manufacturer'],
               'listing_title': listing['title']
@@ -14,23 +17,34 @@ def load_results(json_file_path):
           ]
   return DataFrame(joins).reset_index()
 
-def compare_with_entrant(entrant_folder_name):
+def compare_with_entrant(entrant_folder_name, new_line_offset):
+  columns_to_compare = ['product_name', 'listing_manufacturer', 'listing_title']
+  
   my_file = "../data/output/results.txt"
-  mine = load_results(my_file)
+  mine = load_results(my_file, 0).drop_duplicates(columns_to_compare)
   
   their_file = "../data/comparison/" + entrant_folder_name + "/results.txt"
-  theirs = load_results(their_file)
+  theirs = load_results(their_file, new_line_offset).drop_duplicates(columns_to_compare)
 
-  comparison = pd.merge(left=mine, right=theirs, how='outer', on=['product_name', 'listing_manufacturer', 'listing_title'], suffixes=['_mine','_theirs'])
+  comparison = pd.merge(left=mine, right=theirs, how='outer', on=columns_to_compare, suffixes=['_mine','_theirs'])
 
   their_unique_matches = comparison[comparison['index_mine'].isnull()]
   my_unique_matches = comparison[comparison['index_theirs'].isnull()]
+  
+  common_match_count = len(comparison.index) - len(my_unique_matches) - len(their_unique_matches)
+  
+  print '|entrant | common matches | my unique matches | their unique matches |'
+  print '|---|---|---|---|'
+  print '|' + entrant_folder_name + ' | ' + str(common_match_count) + ' | ' + str(len(my_unique_matches)) + ' | ' + str(len(their_unique_matches)) + ' |'
+  print
   
   their_unique_matches[['product_name', 'listing_manufacturer', 'listing_title']].to_csv("../data/comparison/" + entrant_folder_name + "/their_unique_listings.csv", encoding='utf-8')
   my_unique_matches[['product_name', 'listing_manufacturer', 'listing_title']].to_csv("../data/comparison/" + entrant_folder_name + "/my_unique_listings.csv", encoding='utf-8')
 
 # Compare with other entrants:
-compare_with_entrant('alex_black')
+compare_with_entrant('alex_black', 0)
 
-# Following doesn't work, as each json object doesn't fit on a single line...
-# compare_with_entrant('aaron_levin')
+# Aaron Levin's original results.txt file caused issues as each JSON object covered many lines. 
+# Additionally there was an extra invalid double quote character next to the "title" field.
+# This was first normalized to a single correct JSON object per line using the data/comparison/aaron_levin/normalize-json.ps1 Powershell script.
+compare_with_entrant('aaron_levin', 1)
